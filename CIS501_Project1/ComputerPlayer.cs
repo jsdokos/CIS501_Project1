d8eff40946ffee63c13f495da5fa5ad77ea294f3
jsdokos <jsdokos@gmail.com>
2016-09-19 17:26:47 -0500

    Working on stubbing out program

---
+++ CIS501_Project1/ComputerPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS501_Project1
+{
+    
+    class ComputerPlayer : Player
+    {
+    }
+}
---
+++ CIS501_Project1/PlayingCard.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS501_Project1
+{
+    class PlayingCard
+    {
+        enum CardSuit
+        {
+            Joker,
+            Hearts,
+            Diamonds,
+            Clubs,
+            Spades
+        }
+
+        private CardSuit Suit { get; }
+
+        private int Rank { get; }
+
+        private bool FaceUp { get; }
+
+        public override string ToString()
+        {
+            if (FaceUp)
+            {
+                return Suit.ToString() + Rank;
+            }
+            else
+            {
+                return "XX";
+            }
+    }
+}
